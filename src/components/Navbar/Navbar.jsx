c05d8acf3c4d8926b65d0a59e5a05b7cdd7c8b72
Jonny Jackson <jrsjackson26@gmail.com>
2024-11-15 14:05:14 -0700

    functioning dropdown in navbar for mobile, looks bad

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaSun, FaMoon } from 'react-icons/fa';
 import { useTheme } from '../../context/ThemeContext';
@@ -10,10 +10,15 @@ const Navbar = () => {
   const { isDarkMode, toggleTheme } = useTheme(); // Access theme context
   const { level, setLevel } = useLevel(); // Access level context
   const location = useLocation(); // Get the current location
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track menu open/close
 
   const handleLevelChange = (event) => {
     const selectedLevel = parseInt(event.target.value, 10);
     setLevel(selectedLevel);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle the menu visibility
   };
 
   return (
@@ -25,7 +30,6 @@ const Navbar = () => {
       {/* Render the level dropdown only if the current path is the home page */}
       {location.pathname === '/' && (
         <div className="navbar-level">
-          <label htmlFor="level">Level:</label>
           <select id="level" name="level" value={level} onChange={handleLevelChange}>
             {level_info.map((level) => (
               <option key={level.level} value={level.level}>
@@ -36,14 +40,34 @@ const Navbar = () => {
         </div>
       )}
 
-      <div className="navbar-links">
+      {/* Desktop Links */}
+      <div className="navbar-links desktop">
         {location.pathname !== '/' && (
           <Link to="/">Tutorial</Link>
         )}
         <Link to="/about">About</Link>
         <Link to="/cheat-sheet">Cheat Sheet</Link>
         <Link to="/live-editor">Live Editor</Link>
-        {/*<Link to="/what-is-markdown">What is Markdown?</Link>*/}
+        <div className="theme-switcher" onClick={toggleTheme}>
+          {isDarkMode ? <FaSun className="icon" /> : <FaMoon className="icon" />}
+        </div>
+      </div>
+
+      {/* Hamburger Icon (only for mobile) */}
+      <button className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+      {/* Mobile Menu (only visible when menu is open) */}
+      <div className={`navbar-links mobile ${isMenuOpen ? 'active' : ''}`}>
+        {location.pathname !== '/' && (
+          <Link to="/">Tutorial</Link>
+        )}
+        <Link to="/about">About</Link>
+        <Link to="/cheat-sheet">Cheat Sheet</Link>
+        <Link to="/live-editor">Live Editor</Link>
         <div className="theme-switcher" onClick={toggleTheme}>
           {isDarkMode ? <FaSun className="icon" /> : <FaMoon className="icon" />}
         </div>

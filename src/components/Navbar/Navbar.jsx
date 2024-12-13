9376404aada506a1b1a80a63c61069e0d8ad564f
Jonny Jackson <jrsjackson26@gmail.com>
2024-12-13 16:18:57 -0700

    improving topbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import NavbarLinks from "./NavbarLinks";
@@ -6,15 +6,33 @@ import LevelSelector from "./LevelSelector";
 import ThemeToggle from "./ThemeToggle";
 
 const Navbar = ({ level, setLevel }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState); // Toggle the state
+  };
+
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-title">
         learn-markdown-game
       </Link>
+
+      <LevelSelector level={level} setLevel={setLevel} />
+
       <div className="navbar-right">
-        <LevelSelector level={level} setLevel={setLevel} />
         <NavbarLinks />
         <ThemeToggle />
+      </div>
+
+      {/* Hamburger Icon for mobile */}
+      <div className="navbar-hamburger" onClick={toggleMenu}>
+        <span className="navbar-hamburger-icon">&#9776;</span>
+      </div>
+
+      {/* Dropdown menu for mobile */}
+      <div className={`navbar-dropdown ${isMenuOpen ? "open" : ""}`}>
+        <NavbarLinks />
       </div>
     </nav>
   );
